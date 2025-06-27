c6dc6d7cf458a8385295c0ad848193ab7c0093ab
cciaravolo <ciro.ciaravolo@eng.it>
2025-06-27 16:04:18 +0200

    deleted old json

--- src/main/java/it/eng/dome/revenue/engine/controller/RevenueControllerTest.java
+++ src/main/java/it/eng/dome/revenue/engine/controller/RevenueControllerTest.java
@@ -1,7 +1,10 @@
 package it.eng.dome.revenue.engine.controller;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,9 +20,10 @@ public class RevenueControllerTest {
     private final SubscriptionPlanLoader loader = new SubscriptionPlanLoader();
 
 
-    @GetMapping("/api/plan")
-    public ResponseEntity<SubscriptionPlan> getPlan() throws Exception {
-        SubscriptionPlan plan = loader.loadFromClasspath("sample_data/ProPlan.json");
+    @GetMapping("/api/plan/{id}")
+    public ResponseEntity<SubscriptionPlan> getPlanById(@PathVariable String id) throws Exception {
+        String fileName = "sample_data/" + id + ".json";
+        SubscriptionPlan plan = loader.loadFromClasspath(fileName);
         return ResponseEntity.ok(plan);
     }
 
@@ -27,4 +31,7 @@ public class RevenueControllerTest {
     public ResponseEntity<String> createPlan(@RequestBody SubscriptionPlan plan) {
         return ResponseEntity.ok("Plan received: " + plan.getName());
     }
+
+    
+    
 }
--- src/main/java/it/eng/dome/revenue/engine/service/SubscriptionPlanLoader.java
+++ src/main/java/it/eng/dome/revenue/engine/service/SubscriptionPlanLoader.java
@@ -1,13 +1,20 @@
 package it.eng.dome.revenue.engine.service;
 
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Service;
+
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import it.eng.dome.revenue.engine.model.SubscriptionPlan;
-
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.stereotype.Service;
  
 @Service
 public class SubscriptionPlanLoader {
@@ -25,4 +32,7 @@ public class SubscriptionPlanLoader {
             return mapper.readValue(in, SubscriptionPlan.class);
         }
     }
+    
+    
+
 }
