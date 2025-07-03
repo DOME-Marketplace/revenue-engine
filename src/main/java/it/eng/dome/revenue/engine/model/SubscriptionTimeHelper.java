138fdf20c38d94e90884364293418333668768e3
Paolo Fabriani <paolo.fabriani@eng.it>
2025-07-03 16:00:31 +0200

    more on TimeHelper

--- src/main/java/it/eng/dome/revenue/engine/model/SubscriptionTimeHelper.java
+++ src/main/java/it/eng/dome/revenue/engine/model/SubscriptionTimeHelper.java
@@ -5,15 +5,9 @@ import java.time.OffsetDateTime;
 public class SubscriptionTimeHelper {
 
     private Subscription subscription;
-    private Price price;
-
-    public SubscriptionTimeHelper(Subscription subscription, Price price) {
-        this.subscription = subscription;
-        this.price = price;
-    }
 
     public SubscriptionTimeHelper(Subscription subscription) {
-        this(subscription, null);
+        this.subscription = subscription;
     }
 
     // Compute the subscription period at a given time
@@ -23,16 +17,69 @@ public class SubscriptionTimeHelper {
         if(time.isBefore(this.subscription.getStartDate()))
             return null;
 
-        // iterate over the subscription periods, untile found one that contains the time
+        // iterate over the subscription periods, until found one that contains the time
         OffsetDateTime start = this.subscription.getStartDate();
-        OffsetDateTime now = OffsetDateTime.now();
-        while(start.isBefore(now)) {
+        while(!start.isAfter(time)) {
             OffsetDateTime end = this.rollSubscriptionPeriod(start, 1);
-            if(time.isAfter(start) && time.isBefore(end)) {
+            if(!time.isBefore(start) && time.isBefore(end)) {
                 return new TimePeriod(start, end);
             }
+            start = end;
         }
         return null;
+    }
+
+    public TimePeriod getSubscriptionPeriodByOffset(OffsetDateTime time, Integer howManyPeriods) {
+        OffsetDateTime shiftedTime = this.rollSubscriptionPeriod(time, howManyPeriods);
+        return this.getSubscriptionPeriodAt(shiftedTime);
+    }
+
+    // compute the previous subscription period wrt given time
+    public TimePeriod getPreviousSubscriptionPeriod(OffsetDateTime time) {
+        return this.getSubscriptionPeriodByOffset(time, -1);
+    }
+
+        // compute the previous subscription period wrt given time
+    public TimePeriod getNextSubscriptionPeriod(OffsetDateTime time) {
+        return this.getSubscriptionPeriodByOffset(time, 1);
+    }
+
+    // Compute the subscription period at a given time
+    public TimePeriod getChargePeriodAt(OffsetDateTime time, Price price) {
+
+        // if the time is before the start of the subscription, return null
+        if(time.isBefore(this.subscription.getStartDate()))
+            return null;
+
+        // iterate over the charge periods, until found one that contains the time
+        OffsetDateTime start = this.subscription.getStartDate();
+        while(!start.isAfter(time)) {
+            OffsetDateTime end = this.rollChargePeriod(start, price, 1);
+            System.out.println("Checking period: " + start + " - " + end);
+            if(!time.isBefore(start) && time.isBefore(end)) {
+                return new TimePeriod(start, end);
+            }
+            start = end;
+        }
+        return null;
+    }
+
+    public TimePeriod getChargePeriodByOffset(OffsetDateTime time, Price price, Integer howManyPeriods) {
+        OffsetDateTime shiftedTime = this.rollChargePeriod(time, price, howManyPeriods);
+        System.out.println("Shifted time: " + shiftedTime);
+        return this.getChargePeriodAt(shiftedTime, price);
+    }
+
+    public TimePeriod getCurrentChargePeriod(Price price) {
+        return this.getChargePeriodAt(OffsetDateTime.now(), price);
+    }
+
+    public TimePeriod getPreviousChargePeriod(OffsetDateTime time, Price price) {
+        return this.getChargePeriodByOffset(time, price, -1);
+    }
+
+    public TimePeriod getNextChargePeriod(OffsetDateTime time, Price price) {
+        return this.getChargePeriodByOffset(time, price, 1);
     }
 
     private OffsetDateTime rollSubscriptionPeriod(OffsetDateTime time, int howManyPeriods) {
@@ -53,62 +100,60 @@ public class SubscriptionTimeHelper {
         }
         throw new IllegalArgumentException("Unknown RecurringPeriod type: " + pType);
     }
-
-    // compute the current subscription period
-    public TimePeriod getCurrentSubscriptionPeriod() {
-        // FIXME: remove the following. Just to avoid PMD complaining about unused variables
-        this.price.getRecurringChargePeriodLength();
-
-        return this.getSubscriptionPeriodAt(OffsetDateTime.now());
+    private OffsetDateTime rollChargePeriod(OffsetDateTime time, Price price, int howManyPeriods) {
+        // retrive subscriptino length unit
+        RecurringPeriod pType = price.getRecurringChargePeriodType();
+        // retrieve subscription length
+        Integer pLength = price.getRecurringChargePeriodLength();
+        // ensure the two above are set
+        if(pType == null || pLength == null) {
+            throw new IllegalArgumentException("Price does not have a valid recurring charge period type or length");
+        }
+        // increase according to pType and pLength and howManyPeriods
+        switch(pType) {
+            case DAY:
+                return time.plusDays(pLength * howManyPeriods);
+            case WEEK:
+                return time.plusWeeks(pLength * howManyPeriods);
+            case MONTH:
+                return time.plusMonths(pLength * howManyPeriods);
+            case YEAR:
+                return time.plusYears(pLength * howManyPeriods);
+        }
+        throw new IllegalArgumentException("Unknown RecurringPeriod type: " + pType);
     }
 
-    // compute the previous subscription period wrt given time
-    // if the period is before the start of the subscription, return null
-    // TODO: in any case, clip to the start of the subscription
-    public TimePeriod getPreviousSubscriptionPeriod(OffsetDateTime time) {
-        OffsetDateTime onePeriodAgo = time.minusYears(1); // FIXME: go back to the previous period. Use contractual duration length, etc...
-        // TODO: if before the start of the subscription, return null
-        return this.getSubscriptionPeriodAt(onePeriodAgo);
-    }
+    public static void main(String[] args) throws Exception{
 
-    // compute the previous subscription period
-    // if the current time is before the start of the subscription, return null
-    public TimePeriod getPreviousSubscriptionPeriod() {
-        return this.getPreviousSubscriptionPeriod(OffsetDateTime.now());
-    }
-    // compute the charge period at a given time
-    // considering the given price (with chargeperiod, offset and length)
-    public TimePeriod getChargePeriod(OffsetDateTime time) {
-        return null; // TODO: implement me
-    }
-
-    public TimePeriod getCurrentChargePeriod() {
-        return this.getChargePeriod(OffsetDateTime.now());
-    }
-
-    public TimePeriod getPreviousChargePeriod(OffsetDateTime time) {
-        OffsetDateTime oneChargePeriodAgo = time.minusYears(1); // FIXME: go back to the previous period. Use charge duration, etc...
-        return this.getChargePeriod(oneChargePeriodAgo);
-    }
-
-    public TimePeriod getPreviousChargePeriod() {
-        return this.getPreviousChargePeriod(OffsetDateTime.now());
-    }
-
-    public static void main(String[] args) {
-
+        // create a fake subscription plan
         SubscriptionPlan plan = new SubscriptionPlan();
         plan.setContractDurationPeriodType(RecurringPeriod.YEAR);
-        plan.setContractDurationLength(10);
+        plan.setContractDurationLength(1);
 
-        SubscriptionActive subscription = new SubscriptionActive();
+        // subscribe to the plan
+        Subscription subscription = new Subscription();
         subscription.setPlan(plan);
-
+        subscription.setStartDate(OffsetDateTime.now().minusMonths(8)); // start 8 months ago
+        System.out.println("Subscription start date: " + subscription.getStartDate());
+        // create an helper
         SubscriptionTimeHelper helper = new SubscriptionTimeHelper(subscription);
 
+        // go ahead one subscription period from now
         OffsetDateTime now = OffsetDateTime.now();
-        OffsetDateTime end = helper.rollSubscriptionPeriod(now, 1);
-        System.out.println(end);
+        OffsetDateTime rolledTime = helper.rollSubscriptionPeriod(now, 2);
+        System.out.println(rolledTime);
+
+        // now create a price with a charge period
+        Price price = new Price();
+        price.setRecurringChargePeriodType(RecurringPeriod.MONTH);
+        price.setRecurringChargePeriodLength(1);
+
+        Thread.sleep(1000);
+        now = OffsetDateTime.now();
+
+        TimePeriod chargePeriod = helper.getChargePeriodByOffset(now, price, 18);
+        System.out.println(chargePeriod.getFromDate());
+        System.out.println(chargePeriod.getToDate());
     }
 
 }
