0fee6663dbb064c5c6692714486633145c07b049
Paolo Fabriani <paolo.fabriani@eng.it>
2025-07-09 18:16:30 +0200

    empty statements for real subscriptions and plans

--- src/main/java/it/eng/dome/revenue/engine/controller/SubscriptionsController.java
+++ src/main/java/it/eng/dome/revenue/engine/controller/SubscriptionsController.java
@@ -1,9 +1,7 @@
 package it.eng.dome.revenue.engine.controller;
 
-import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,19 +15,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 
-import it.eng.dome.revenue.engine.model.Price;
-import it.eng.dome.revenue.engine.model.RecurringPeriod;
 import it.eng.dome.revenue.engine.model.RevenueStatement;
 import it.eng.dome.revenue.engine.model.Subscription;
 import it.eng.dome.revenue.engine.model.SubscriptionPlan;
 import it.eng.dome.revenue.engine.model.SubscriptionTimeHelper;
 import it.eng.dome.revenue.engine.model.TimePeriod;
+import it.eng.dome.revenue.engine.service.SubscriptionPlanService;
+import it.eng.dome.revenue.engine.service.SubscriptionService;
 import it.eng.dome.revenue.engine.service.TmfDataRetriever;
-import it.eng.dome.tmforum.tmf632.v4.model.Organization;
 
 @RestController
 //@RequiredArgsConstructor
-@RequestMapping("/dev2/revenue")
+@RequestMapping("/dev2/revenue/subscriptions")
 public class SubscriptionsController {
     
 	protected final Logger logger = LoggerFactory.getLogger(StatementsController.class);
@@ -38,14 +35,26 @@ public class SubscriptionsController {
     TmfDataRetriever tmfDataRetriever;
 
     @Autowired
-    public SubscriptionsController() {
+    SubscriptionService subscriptionService;
+    SubscriptionPlanService subscriptionPlanService;
+
+    @Autowired
+    public SubscriptionsController(SubscriptionPlanService subscriptionPlanService,
+                              SubscriptionService subscriptionService /*, ObjectMapper mapper*/) {
+        this.subscriptionPlanService = subscriptionPlanService;
+        this.subscriptionService = subscriptionService;
+//        this.mapper = mapper;
     }
 
-    @GetMapping("/subscriptions/")
+    @GetMapping("/")
     public ResponseEntity<List<Subscription>> getAllSubscriptions() {
         try {
+            /*
             List<Subscription> subscriptions = new ArrayList<>();
             subscriptions.add(this.createFakeSubscription(UUID.randomUUID().toString()));
+            return ResponseEntity.ok(subscriptions);
+            */
+            List<Subscription> subscriptions = subscriptionService.loadAllFromStorage();
             return ResponseEntity.ok(subscriptions);
         } catch (Exception e) {
             logger.error(e.getMessage(), e);
@@ -53,28 +62,38 @@ public class SubscriptionsController {
         }
     }
 
-    @GetMapping("/subscriptions/{subscriptionId}")
+    @GetMapping("/{subscriptionId}")
     public ResponseEntity<Subscription> getSubscription(@PathVariable String subscriptionId) {
-        if(Math.random() < 0.1) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        }
         try {
-            return ResponseEntity.ok(this.createFakeSubscription(subscriptionId));
+            Subscription subscription = subscriptionService.getBySubscriptionId(subscriptionId);
+            if (subscription == null) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+            }
+            else {
+                return ResponseEntity.ok(subscription);
+            }
         } catch (Exception e) {
             logger.error(e.getMessage(), e);
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
 
-    @GetMapping("/subscriptions/{subscriptionId}/statements")
+    @GetMapping("/{subscriptionId}/statements")
     public ResponseEntity<List<RevenueStatement>> sellerStatements(@PathVariable String subscriptionId) {
         try {
             List<RevenueStatement> statements = new ArrayList<>();
 
-            Subscription subscription = this.createFakeSubscription(subscriptionId);
+            // retrieve the subscription by id
+            Subscription subscription = subscriptionService.getBySubscriptionId(subscriptionId);
 
+            // retrive the plan for the subscription
+            SubscriptionPlan plan = this.subscriptionPlanService.findPlanById(subscription.getPlan().getId());
+
+            // add the plan to the subscription
+            subscription.setPlan(plan);
+
+            // build empty, fake statements
             SubscriptionTimeHelper timeHelper = new SubscriptionTimeHelper(subscription);
-
             for(TimePeriod tp : timeHelper.getChargePeriodTimes()) {
                 statements.add(new RevenueStatement(subscription, tp));
             }
@@ -84,65 +103,7 @@ public class SubscriptionsController {
             logger.error(e.getMessage(), e);
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
-    }
 
-    @GetMapping("/organizations/{referrerOrganizationId}/referrals")
-    public ResponseEntity<List<Organization>> listReferralsProviders(@PathVariable String referrerOrganizationId) {
-        try {
-            return ResponseEntity.ok(tmfDataRetriever.listReferralsProviders(referrerOrganizationId));
-        } catch (Exception e) {
-            logger.error(e.getMessage(), e);
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
-    }
-
-    @GetMapping("/organizations/{referralOrganizationId}/referrer")
-    public ResponseEntity<Organization> getReferrerProvider(@PathVariable String referralOrganizationId) {
-        try {
-            return ResponseEntity.ok(tmfDataRetriever.getReferrerProvider(referralOrganizationId));
-        } catch (Exception e) {
-            logger.error(e.getMessage(), e);
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
-    }
-
-    private Subscription createFakeSubscription(String subscriptionId) {
-        Subscription subscription = new Subscription();
-        subscription.setName("Fake Subscription");
-        subscription.setStatus("ACTIVE");
-        subscription.setId(subscriptionId);
-        subscription.setStartDate(OffsetDateTime.now().minusMonths(47));
-        subscription.setPlan(this.createFakeSubscriptionPlan(UUID.randomUUID().toString()));
-        subscription.setRelatedParties(new ArrayList<>());
-
-        return subscription;
-    }
-
-    private SubscriptionPlan createFakeSubscriptionPlan(String planId) {
-        SubscriptionPlan plan = new SubscriptionPlan();
-        plan.setId(planId);
-        plan.setName("Fake Plan");
-        plan.setDescription("This is a test subscription plan.");
-        plan.setContractDurationPeriodType(RecurringPeriod.YEAR);
-        plan.setContractDurationLength(1);
-
-        Price bundle = new Price();
-        bundle.setName("Bundle Price");
-        bundle.setIsBundle(true);
-
-        Price price = new Price();
-        price.setRecurringChargePeriodType(RecurringPeriod.YEAR);
-        price.setRecurringChargePeriodLength(1);
-
-        Price anotherPrice = new Price();
-        anotherPrice.setRecurringChargePeriodType(RecurringPeriod.MONTH);
-        anotherPrice.setRecurringChargePeriodLength(13);
-
-        List<Price> prices = List.of(price, anotherPrice);
-        bundle.setPrices(prices);
-        plan.setPrice(bundle);
-
-        return plan;
     }
 
 }
