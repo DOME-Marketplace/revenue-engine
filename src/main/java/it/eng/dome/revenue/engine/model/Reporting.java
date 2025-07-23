1f804daa36c9c7bfd06999392c29092a2acc5568
Di Zenzo Carmine Federico <CarmineFederico.DiZenzo@eng.it>
2025-07-23 11:33:17 +0200

    Create Reporting.java

---
+++ src/main/java/it/eng/dome/revenue/engine/model/Reporting.java
@@ -0,0 +1,57 @@
+package it.eng.dome.revenue.engine.model;
+
+import java.util.List;
+
+public class Reporting {
+
+    private String label;			// key
+    private String text;            // value
+    private String link;            // optional
+    private List<Reporting> items;  // recursive
+
+    public Reporting() {}
+
+    public Reporting(String label, String text) {
+        this.label = label;
+        this.text = text;
+    }
+
+    public Reporting(String label, String text, String link) {
+        this.label = label;
+        this.text = text;
+        this.link = link;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public List<Reporting> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Reporting> items) {
+        this.items = items;
+    }
+}
+
