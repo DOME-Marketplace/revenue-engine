fee19699afa3e6a7e5477a0a351ae3924b12753d
Paolo Fabriani <paolo.fabriani@eng.it>
2025-07-24 18:23:10 +0200

    rest endpoint for statements items only

--- src/main/java/it/eng/dome/revenue/engine/model/RevenueStatement.java
+++ src/main/java/it/eng/dome/revenue/engine/model/RevenueStatement.java
@@ -1,5 +1,6 @@
 package it.eng.dome.revenue.engine.model;
 
+import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -59,6 +60,17 @@ public class RevenueStatement {
             this.revenueItems = new ArrayList<>();
         }
         this.revenueItems.add(item);
+    }
+
+    public void clusterizeItems() {
+        List<RevenueItem> newItems = new ArrayList<>();
+        // replace items with filtered ones
+        for(RevenueItem item : this.getRevenueItems()) {
+            for(OffsetDateTime chargeTime : item.extractChargeTimes()) {
+                newItems.add(item.getFilteredClone(chargeTime));
+            }
+        }
+        this.setRevenueItems(newItems);
     }
 
 }
