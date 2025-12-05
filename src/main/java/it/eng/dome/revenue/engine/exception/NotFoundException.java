2fc971eb6688a6cb11849c039c254c904067fb87
pasquy <pasquale.vitale@gmail.com>
2025-12-05 13:46:17 +0100

    clean up

--- src/main/java/it/eng/dome/revenue/engine/exception/NotFoundException.java
+++ src/main/java/it/eng/dome/revenue/engine/exception/NotFoundException.java
@@ -5,7 +5,10 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) {
+
+	private static final long serialVersionUID = -2359903864142722447L;
+
+	public NotFoundException(String message) {
         super(message);
     }
 }
--- src/main/java/it/eng/dome/revenue/engine/service/cached/TmfCachedDataRetriever.java
+++ src/main/java/it/eng/dome/revenue/engine/service/cached/TmfCachedDataRetriever.java
@@ -6,7 +6,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
 
-import it.eng.dome.brokerage.api.fetch.FetchUtils;
 import org.ehcache.Cache;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
